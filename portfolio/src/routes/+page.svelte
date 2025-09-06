453036f6bdc6f42f44d555fd13f337b91f3cf2d3
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-09-06 14:45:11 -0700

    add project 1 + add about me picture

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -15,12 +15,22 @@
 <script>
   import NavBar from '../components/NavBar.svelte';
   import ProjectPreview from '../components/ProjectPreview.svelte';
+  import AboutMeImage from '$lib/assets/about-me.png';
+
+
   let buttons = [
     { name: 'Home', link: '#home' },
     { name: 'Work', link: '#work' },
     { name: 'Extras', link: '#extras' },
     { name: 'Contact', link: '#contact' },
   ];
+
+  let project1 = {
+    title: "FullyBeyond",
+    project_duration: "7 Months",
+    role: "Web Lead, Co-Host",
+    problem_statement: "Students interested in design had no dedicated platform to collaborate and showcase their skills in a hackathon-style environment."
+  };
 </script>
 
 <div class="text-[#000000] bg-[#FFFFFF] min-h-screen p-8">
@@ -45,7 +55,7 @@
 
   <div id="work" class="mt-44 m-10">
     <p class="text-5xl font-sans">Case Studies</p>
-    <ProjectPreview />
+    <ProjectPreview {...project1} />
     <a href="/previous" class="text-2xl mt-6 inline-block relative">
       ← <span
         class="relative after:content-[''] after:absolute after:w-full after:h-0.5 
@@ -74,18 +84,26 @@
     <p class="text-5xl font-sans">About Me</p>
 
     <div class="flex flex-col md:flex-row items-center md:items-start justify-between pt-12">
-    <div class="w-80 h-96 border-2 shadow-lg -rotate-5"></div>
-    <p class="text-3xl leading-relaxed font-sans w-2/3">
-      I’m a senior at Cal State Fullerton majoring in Computer Science, where I’ve had the opportunity to lead teams, co-direct FullyHacks 2025, and serve as web lead for FullyBeyond 2024, our first student-run designathon. Beyond school, I spent the summer as a User Experience Researcher at LPL Financial, where I discovered how much I enjoy <em>shaping products by connecting user insights to real design and engineering decisions</em>.
-      What excites me most about product management is being part of the <em>entire journey</em> — from defining a problem to seeing a solution make a difference for users. I love collaborating with diverse teams, <em>asking the right questions</em>, and turning ideas into products that people actually enjoy using.
-    </p>
-    </div>
-    <a href="https://samahmed.info" target="_blank" class="text-2xl mt-10 inline-block relative float-right"><span class="inline-block relative 
-      after:content-[''] after:absolute after:w-full after:h-0.5 
-      after:bottom-0 after:left-0 after:scale-x-0 after:origin-bottom-right 
-      after:transition-transform after:duration-[250ms] after:ease-out after:bg-[#000000]
-      hover:after:scale-x-100 hover:after:origin-bottom-left">Discover Technical & Creative Portfolio</span> →</a>
+
+      <div class="w-80 h-96 border-2 shadow-lg -rotate-5 flex items-center justify-center">
+        <img src={AboutMeImage} alt="About Me Image" class="w-72 h-80 object-cover" />
+      </div>
+
+      <p class="text-3xl leading-relaxed font-sans w-2/3">
+        I’m a senior at Cal State Fullerton majoring in Computer Science, where I’ve had the opportunity to lead teams, co-direct FullyHacks 2025, and serve as web lead for FullyBeyond 2024, our first student-run designathon. Beyond school, I spent the summer as a User Experience Researcher at LPL Financial, where I discovered how much I enjoy <em>shaping products by connecting user insights to real design and engineering decisions</em>.
+        What excites me most about product management is being part of the <em>entire journey</em> — from defining a problem to seeing a solution make a difference for users. I love collaborating with diverse teams, <em>asking the right questions</em>, and turning ideas into products that people actually enjoy using.
+      </p>
+      </div>
+      <a href="https://samahmed.info" target="_blank" class="text-2xl mt-10 inline-block relative float-right"><span class="inline-block relative 
+        after:content-[''] after:absolute after:w-full after:h-0.5 
+        after:bottom-0 after:left-0 after:scale-x-0 after:origin-bottom-right 
+        after:transition-transform after:duration-[250ms] after:ease-out after:bg-[#000000]
+        hover:after:scale-x-100 hover:after:origin-bottom-left">Discover Technical & Creative Portfolio</span> →</a>
     
+    </div>
+
+  <div id="extras" class="mt-44 m-10">
+    <p class="text-5xl font-sans">My College Journey</p>
   </div>
 
 
