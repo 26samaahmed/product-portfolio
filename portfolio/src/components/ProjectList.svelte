df014b53974fd1443a72d9e16ba4b53c7ad9f97c
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-09-06 19:49:53 -0700

    reorganize code

---
+++ portfolio/src/components/ProjectList.svelte
@@ -0,0 +1,35 @@
+<script>
+  import ProjectPreview from '../components/ProjectPreview.svelte';
+  
+  let project1 = {
+    title: "FullyBeyond",
+    project_duration: "7 Months",
+    role: "Web Lead, Co-Host",
+    problem_statement: "Students interested in design had no dedicated platform to collaborate and showcase their skills in a hackathon-style environment."
+};
+
+</script>
+
+<p class="text-5xl font-sans">Case Studies</p>
+<ProjectPreview {...project1} />
+<a href="/previous" class="text-2xl mt-6 inline-block relative">
+  ← <span
+    class="relative after:content-[''] after:absolute after:w-full after:h-0.5 
+      after:bottom-0 after:left-0 after:scale-x-0 after:origin-bottom-right 
+      after:transition-transform after:duration-[250ms] after:ease-out after:bg-[#000000]
+      hover:after:scale-x-100 hover:after:origin-bottom-left"
+  >
+    Previous Project
+  </span>
+</a>
+
+<a href="/next" class="text-2xl mt-6 inline-block relative float-right">
+  <span
+    class="relative after:content-[''] after:absolute after:w-full after:h-0.5 
+      after:bottom-0 after:left-0 after:scale-x-0 after:origin-bottom-right 
+      after:transition-transform after:duration-[250ms] after:ease-out after:bg-[#000000]
+      hover:after:scale-x-100 hover:after:origin-bottom-left"
+  >
+    Next Project
+  </span> →
+</a>
--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -14,8 +14,8 @@
 
 <script>
   import NavBar from '../components/NavBar.svelte';
-  import ProjectPreview from '../components/ProjectPreview.svelte';
-  import AboutMeImage from '$lib/assets/about-me.png';
+  import ProjectList from '../components/ProjectList.svelte';
+  import AboutMe from '../components/AboutMe.svelte';
 
 
   let buttons = [
@@ -25,12 +25,6 @@
     { name: 'Contact', link: '#contact' },
   ];
 
-  let project1 = {
-    title: "FullyBeyond",
-    project_duration: "7 Months",
-    role: "Web Lead, Co-Host",
-    problem_statement: "Students interested in design had no dedicated platform to collaborate and showcase their skills in a hackathon-style environment."
-  };
 </script>
 
 <div class="text-[#000000] bg-[#FFFFFF] min-h-screen p-8">
@@ -54,53 +48,12 @@
   </div>
 
   <div id="work" class="mt-44 m-10">
-    <p class="text-5xl font-sans">Case Studies</p>
-    <ProjectPreview {...project1} />
-    <a href="/previous" class="text-2xl mt-6 inline-block relative">
-      ← <span
-        class="relative after:content-[''] after:absolute after:w-full after:h-0.5 
-          after:bottom-0 after:left-0 after:scale-x-0 after:origin-bottom-right 
-          after:transition-transform after:duration-[250ms] after:ease-out after:bg-[#000000]
-          hover:after:scale-x-100 hover:after:origin-bottom-left"
-      >
-        Previous Project
-      </span>
-    </a>
-    
-    <a href="/next" class="text-2xl mt-6 inline-block relative float-right">
-      <span
-        class="relative after:content-[''] after:absolute after:w-full after:h-0.5 
-          after:bottom-0 after:left-0 after:scale-x-0 after:origin-bottom-right 
-          after:transition-transform after:duration-[250ms] after:ease-out after:bg-[#000000]
-          hover:after:scale-x-100 hover:after:origin-bottom-left"
-      >
-        Next Project
-      </span> →
-    </a>
-    
+    <ProjectList />
   </div>
 
   <div id="extras" class="mt-44 m-10">
-    <p class="text-5xl font-sans">About Me</p>
-
-    <div class="flex flex-col md:flex-row items-center md:items-start justify-between pt-12">
-
-      <div class="w-80 h-96 border-2 shadow-lg -rotate-5 flex items-center justify-center">
-        <img src={AboutMeImage} alt="About Me Image" class="w-72 h-80 object-cover" />
-      </div>
-
-      <p class="text-3xl leading-relaxed font-sans w-2/3">
-        I’m a senior at Cal State Fullerton majoring in Computer Science, where I’ve had the opportunity to lead teams, co-direct FullyHacks 2025, and serve as web lead for FullyBeyond 2024, our first student-run designathon. Beyond school, I spent the summer as a User Experience Researcher at LPL Financial, where I discovered how much I enjoy <em>shaping products by connecting user insights to real design and engineering decisions</em>.
-        What excites me most about product management is being part of the <em>entire journey</em> — from defining a problem to seeing a solution make a difference for users. I love collaborating with diverse teams, <em>asking the right questions</em>, and turning ideas into products that people actually enjoy using.
-      </p>
-      </div>
-      <a href="https://samahmed.info" target="_blank" class="text-2xl mt-10 inline-block relative float-right"><span class="inline-block relative 
-        after:content-[''] after:absolute after:w-full after:h-0.5 
-        after:bottom-0 after:left-0 after:scale-x-0 after:origin-bottom-right 
-        after:transition-transform after:duration-[250ms] after:ease-out after:bg-[#000000]
-        hover:after:scale-x-100 hover:after:origin-bottom-left">Discover Technical & Creative Portfolio</span> →</a>
-    
-    </div>
+    <AboutMe />
+  </div>
 
   <div id="extras" class="mt-44 m-10">
     <p class="text-5xl font-sans">My College Journey</p>
